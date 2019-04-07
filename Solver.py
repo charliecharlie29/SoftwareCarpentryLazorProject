9a97ebded25af2d19c453d5e4371213903b304ff
kmill126 <47227522+kmill126@users.noreply.github.com>
2019-04-07 10:24:57 -0400

    Working permutation iterator
    Will make all permutations of the board. 
    Cycles through each permutation of the board; 
    Can pair with the lasor function (Yi) and exit when a winning board is found

--- Solver.py
+++ Solver.py
@@ -1,4 +1,6 @@
 """
+PYTHON 3.7.2
+
 Katherine Miller
 Last edited: 2019.04.04
 Lazor Project Grid Solver
@@ -8,7 +10,7 @@ It takes the output from the reader.py to construct all possible combinations
 of the board, then checks if the board is a solution via the laser() function.
 **WORK IN PROGRESS**
 """
-
+import copy
 from itertools import *
 from sympy.utilities.iterables import multiset_permutations
                                                            
@@ -18,25 +20,8 @@ from sympy.utilities.iterables import multiset_permutations
 
 # This is what the grid array looks like.
 array = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'], ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'], ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'], ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'x', 'x', 'o', 'x'], ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'], ['x', 'o', 'x', 'x', 'x', 'o', 'x', 'o', 'x', 'o', 'x'], ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']]
+###############
 
-#[[(targetpoint-x,targetpoint-y),(slope,direction)]
-## I'm assuming slope, direction means run,rise
-laser = [[(4,9),(-1,-1)],[(6,9),(-1,-1)]]
-
-## This is a list of the points that we have to intersect to complete the puzzle
-targets = [(2,5),(5,0)]
-################################
-
-## Indexing the array: 
-##    array[0] = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
-##    array[0][0] = 'x'
-##    array[y][x]
-
-
-## Set the target points
-## indicated with 't'
-for point in targets:
-	array[point[1]][point[0]] = 't'
 
 ## Set block locations
 ## Pull out block centers
@@ -57,71 +42,22 @@ for i in range((A+B),(A+B+C)):
 ## Get all permutations of block locations
 permutations = list(multiset_permutations(blockspots))
 
-#use a shorter list to test
-shortened = []
-for i in range(3):
-        shortened.append(permutations[i])
-
-
-###THIS PUTS THE BLOCKS IN THE RIGHT SPOT
-###IT DOESN'T INCORPERATE PERMUTATIONS
-###IT ACTS AS A FAILSAFE IN CASE I MESS IT UP DOWNSTREAM
-#### Put blocks back into board
-##for l in range(length):
-##        for w in range(width):
-##                if array[l][w] == 'o':
-##                        item = blockspots.pop(0)
-##                        array[l][w] = item         
-
-
-
-#####WORKING VERSION
-
-#make a separate variable so not to mess up original        
-array_reset = array
-
-#print to make sure it looks okay
-for i in array_reset:
-        print(i)
-print()
-
 #getting the length and width of the array makes indexing easier
-length = len(array_reset)
-width = len(array_reset[0])
-
-
-whilecount = 0
-forcount = 0
-
+length = len(array)
+width = len(array[0])
 
 
 #for each permutation in the shortened list
-for permutation in shortened:
+for p in permutations:
         #these counts are just to keep track of what is getting looped when
-        print('for ',forcount)
-        forcount += 1
-        innercount = 0
         
-        #first reset the working array back to all 'o's
-        workingarray = array_reset
-        
+        workingarray = copy.deepcopy(array)
         #then loop through the array replacing 'o's with the blocks
         for l in range(length):
                 for w in range(width):
                         if workingarray[l][w] == 'o':
-                                print('inner ',innercount)
-                                innercount += 1
-                                
-                                workingarray[l][w] = permutation.pop(0)
-for i in array:
-        print(i)
-
-
-
-####NOTES:
-####Here the working array isn't resetting.
-####What's worse, the original array is somehow getting written over
-
+                                workingarray[l][w] = p.pop(0)
+        
 
 
 
